262f8f18dc52bb4a1049885aeb4d6ca26c2a74fa
SLAWSS <garanin.vecheslav@mail.ru>
2026-01-20 09:49:25 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,32 +1,28 @@
-"""
-URL configuration for tochkafilms project.
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+app_name = "films"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("films.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("", views.home, name="home"),
+    path("film/<int:pk>/", views.film_detail, name="film_detail"),
+    path("category/<slug:slug>/", views.category_films, name="category_films"),
+    path("search/", views.search, name="search"),
+    path("my-list/", views.my_list, name="my_list"),
+    path("film/<int:pk>/favorite/", views.toggle_favorite, name="toggle_favorite"),
+    path("film/<int:pk>/watchlist/", views.toggle_watchlist, name="toggle_watchlist"),
+    path("film/<int:pk>/review/", views.add_review, name="add_review"),
+    
+    # Актеры
+    path("actors/", views.actors_list, name="actors_list"),
+    path("actor/<int:pk>/", views.actor_detail, name="actor_detail"),
+    path("actors/search/", views.actors_search, name="actors_search"),
+    
+    # Новые функции
+    path("top/", views.top_films, name="top_films"),
+    path("filter/", views.films_filter, name="films_filter"),
+    path("history/", views.view_history, name="view_history"),
+    path("history/clear/", views.clear_history, name="clear_history"),
+    path("notifications/", views.notifications, name="notifications"),
+    path("api/notifications-count/", views.get_notifications_count, name="notifications_count"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    if settings.STATICFILES_DIRS:
-        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
